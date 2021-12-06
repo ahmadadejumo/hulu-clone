e1a07200270aa1f59b826f14f162efe8e133c66e
ahmadadejumo <adejumoahmad@gmail.com>
2021-12-06 16:32:36 +0100

    fetched the data from the tmbdb

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -10,6 +12,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Results results={results}/>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url ||  requests.fetchTrending.url}`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    }
+  }
 }
